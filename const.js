02764fcdf50d687bcf7e866a98318c1623a335d2
Yurpolskiy <fpbidni@gmail.com>
2023-12-06 01:45:01 +0200

    Made pre-alpha bot version

--- const.js
+++ const.js
@@ -1,6 +1,39 @@
 const commands = `
 /start - Restart the bot
 /help - Help
+/weather - weather forecast
 `
 
-module.exports.commands = commands
+const greetings = `
+I'm the weather bot! Please write the city
+and i'll give u the weather forecast!
+`
+
+const temperatureForecast = (temp, feels_like, temp_min, temp_max, humidity) => {
+    return `<b>Temperature info:</b>
+    <i>Temperature: ${temp} C</i>
+    <i>Feels like: ${feels_like} C</i>
+    <i>Minimum temperature: ${temp_min} C</i>
+    <i>Maximum temperature: ${temp_max} C</i>
+    <i>Humidity: ${humidity} %</i>
+`
+}
+
+const weatherForecast = (forecast, description) => {
+    return `<b>Weather info:</b>
+        <i>Forecast: ${forecast}</i>
+        <i>Description: ${description}</i>`
+}
+
+const windForecast = (speed) => {
+    return `<b>Wind info:</b>
+        <i>Speed: ${speed} m/s</i>`
+}
+
+module.exports = {
+    commands,
+    greetings,
+    temperatureForecast,
+    weatherForecast,
+    windForecast
+}
--- index.js
+++ index.js
@@ -1,54 +1,68 @@
-const {Telegraf, Markup} = require('telegraf')
-require('dotenv').config()
-const help = require('./const.js')
+const { Telegraf, Markup } = require('telegraf');
+require('dotenv').config();
+const help = require('./const.js');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'innocent'}!`))
-bot.help((ctx) => ctx.reply(help.commands))
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('course', async (ctx) => {
-    try {
-        await ctx.replyWithHTML('<b>Choose your course:</b>', Markup.inlineKeyboard(
-    [
-            [Markup.button.callback('Gay Button', 'gay')]
-        ]
-    ))
-    } catch (error) {
-        console.log(error)
-    }
-})
 
-function addActionBot(name, src, text) {
-    bot.action(name, async(ctx) => {
-        try {
-            await ctx.answerCbQuery()
-            if(src) {
-                await ctx.replyWithPhoto(src, {
-                  source: src
-                })
-            }
-            await ctx.replyWithHTML(text,{
-                disable_web_page_preview: true
-            })
-        } catch(error) {
-            console.log(error)
-        }
-    })
+let awaitingCity = false; // Перемения для хранения состояния чата
+
+bot.start((ctx) => {
+    const chatId = ctx.chat.id;
+    const messageId = ctx.message.message_id;
+    ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'innocent'}! 
+                ${help.greetings}`);
+});
+
+bot.help((ctx) => ctx.reply(help.commands));
+
+async function fetchApi(city, key) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    const response = await fetch(url);
+    return await response.json();
 }
 
-addActionBot('gay', null, 'Sex with hitler')
-// bot.action('gay', async(ctx) => {
-//     try {
-//         await ctx.answerCbQuery()
-//         await ctx.replyWithHTML('Lublu v jopu',{
-//             disable_web_page_preview: true
-//         })
-//     } catch(error) {
-//         console.log(error)
-//     }
-// })
+bot.command('weather', (ctx) => {
 
-bot.launch()
+    awaitingCity = true;
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+    ctx.reply('Please enter the city for weather information.');
+});
+
+
+bot.on('text', async (ctx) => {
+    try {
+        if (awaitingCity) {
+
+            const city = ctx.message.text;
+
+            awaitingCity = false;
+
+            const data = await fetchApi(city, process.env.API_TOKEN);
+            console.log(data);
+
+            if (data.weather && data.main && data.wind) {
+                const { weather, main, wind } = data;
+                const { temp, feels_like, temp_min, temp_max, humidity } = main;
+                const { speed } = wind;
+                const forecast = weather[0].main;
+                const description = weather[0].description;
+
+                await ctx.replyWithHTML(help.temperatureForecast(temp, feels_like, temp_min, temp_max, humidity));
+                await ctx.replyWithHTML(help.weatherForecast(forecast, description))
+                await ctx.replyWithHTML(help.windForecast(speed))
+             ctx.reply(`That's all; Just write the command /weather to get the weather forecast. I wish you luck :DD`);
+            } else {
+                ctx.reply('City not found, please write another one.');
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
